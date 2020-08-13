00c1669905a2661100387e7b791785eb0fc46369
Kobi Hari <kobi2294@yahoo.com>
2020-08-13 14:41:09 +0300

    Intro to LINQ

---
+++ Day 02/IntroToLinq/IntroToLinq/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var i = 10;
+            //var count = i.HowManyTimes(20, n => n + 1);
+
+            //var countString = "Hello".HowManyTimes("Hellooooooo", t => t + 'o');
+
+            //Console.WriteLine(countString);
+            //Console.ReadLine();
+
+            var items = RandomNumbers(100)
+                    .Zip(RandomNumbers(200), (i, j) => (i, j))
+                    .Take(40)
+                    .OrderBy(pair => pair.i)
+                    .ThenBy(pair => pair.j);
+
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("--------------------------------------------");
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+
+
+        static IEnumerable<int> RandomNumbers(int seed)
+        {
+            var rand = new Random(seed);
+
+            while(true)
+            {
+                yield return rand.Next(1, 100);
+            }
+        }
+    }
+}
---
+++ Day 02/IntroToLinq/IntroToLinq/Tools.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroToLinq
+{
+    public static class Tools
+    {
+        //public static int HowManyTimes<T>(this T start, T target, Func<T, T> step)
+        //    where T: IEquatable<T>
+        //{
+        //    var counter = 0;
+        //    var current = start;
+
+        //    while (!current.Equals(target))
+        //    {
+        //        counter++;
+        //        current = step(current);
+        //    }
+
+        //    return counter;            
+        //}
+
+        //public static IEnumerable<K> Map<T, K>(this IEnumerable<T> source, Func<T, K> projection)
+        //{
+        //    foreach (var item in source)
+        //    {
+        //        var translated = projection(item);
+        //        yield return translated;
+        //    }
+        //}
+
+        //public static IEnumerable<K> TakeFirst<K>(this IEnumerable<K> source, int number)
+        //{
+        //    var counter = 0;
+
+        //    foreach (var item in source)
+        //    {
+        //        counter++;
+        //        if (counter > number) yield break;
+
+        //        yield return item;
+        //    }
+        //}
+    }
+}
