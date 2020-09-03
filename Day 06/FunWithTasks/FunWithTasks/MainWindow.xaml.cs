f1cb7378120cdf944aba8931dcf7a71e43d2cde6
Kobi Hari <kobi2294@yahoo.com>
2020-09-03 09:21:16 +0300

    Starting Day 6

---
+++ Day 06/FunWithTasks/FunWithTasks/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FunWithTasks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnGo_Click(object sender, RoutedEventArgs e)
+        {
+
+            Debug.WriteLine("A");
+            var res = await goAsync();
+            Debug.WriteLine("B");
+        }
+
+        private async Task<string> goAsync()
+        {
+            Debug.WriteLine("1");
+            txtStatus.Text = "Calculating...";
+            btnGo.IsEnabled = false;
+            btnCancel.IsEnabled = true;
+            progress.IsIndeterminate = true;
+
+            Debug.WriteLine("2");
+
+            var results = await PrimesCalculator.GetAllPrimesAsync(2, 180000);
+
+            Debug.WriteLine("3");
+
+            lstResults.ItemsSource = results;
+            txtStatus.Text = "Completed";
+            btnGo.IsEnabled = true;
+            btnCancel.IsEnabled = false;
+            progress.IsIndeterminate = false;
+
+            Debug.WriteLine("4");
+
+            return "Hello";
+        }
+
+        private void btnGo_Click1(object sender, RoutedEventArgs e)
+        {
+            txtStatus.Text = "Calculating...";
+            btnGo.IsEnabled = false;
+            btnCancel.IsEnabled = true;
+            progress.IsIndeterminate = true;
+
+            PrimesCalculator.GetAllPrimesAsync(2, 180000)
+                .ContinueWith(task =>
+                {
+                    var results = task.Result;
+                    lstResults.ItemsSource = results;
+                    txtStatus.Text = "Completed";
+                    btnGo.IsEnabled = true;
+                    btnCancel.IsEnabled = false;
+                    progress.IsIndeterminate = false;
+
+                }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+    }
+}
---
+++ Day 06/FunWithTasks/FunWithTasks/PrimesCalculator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Animation;
+
+namespace FunWithTasks
+{
+    public static class PrimesCalculator
+    {
+        private static bool _isPrime(this int number)
+        {
+            for (int i = 2; i < number ; i++)
+            {
+                if (number % i == 0) return false;
+            }
+
+            return true;
+        }
+
+        private static IEnumerable<int> _getAllPrimes(int start, int finish)
+        {
+            for (int i = start; i <= finish; i++)
+            {
+                if (i._isPrime()) yield return i;
+            }
+        }
+
+        public static List<int> GetAllPrimes(int start, int finish)
+        {
+            return _getAllPrimes(start, finish).ToList();
+        }
+
+        public static Task<List<int>> GetAllPrimesAsync(int start, int finish)
+        {
+            return Task.Factory.StartNew(() => GetAllPrimes(start, finish));
+        }
+
+
+    }
+}
