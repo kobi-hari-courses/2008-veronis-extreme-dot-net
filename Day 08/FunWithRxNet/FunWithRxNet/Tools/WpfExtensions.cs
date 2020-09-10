7021979dda665108e3cbff4c26b9ce37241da66c
Kobi Hari <kobi2294@yahoo.com>
2020-09-10 10:50:50 +0300

    Rx Counter - Part 1 - Using Dispose to release the subscription

---
+++ Day 08/FunWithRxNet/FunWithRxNet/Tools/WpfExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace FunWithRxNet.Tools
+{
+    public static class WpfExtensions
+    {
+        public static IObservable<Unit> ObserveClick(this Button source)
+        {
+            return Observable.FromEventPattern<RoutedEventHandler, RoutedEventArgs>(
+                h => source.Click += h,
+                h => source.Click -= h)
+                .Select(ep => Unit.Default);
+        }
+
+
+    }
+}
---
+++ Day 08/FunWithRxNet/FunWithRxNet/Views/CounterReader.xaml.cs
@@ -0,0 +1,46 @@
+﻿using FunWithRxNet.Services;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FunWithRxNet.Views
+{
+    /// <summary>
+    /// Interaction logic for CounterReader.xaml
+    /// </summary>
+    public partial class CounterReader : UserControl
+    {
+        private IDisposable _subscription = null;
+
+        public CounterReader()
+        {
+            InitializeComponent();
+
+            Unloaded += CounterReader_Unloaded;
+
+            _subscription = CounterService.Instance
+                .GetCounter()
+                .Subscribe(val => {
+                    counterLabel.Text = val.ToString();
+                    Debug.WriteLine($"Counter.OnNext({val})");
+                    });
+        }
+
+        private void CounterReader_Unloaded(object sender, RoutedEventArgs e)
+        {
+            _subscription.Dispose();
+        }
+    }
+}
